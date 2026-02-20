0cfb6fed0a14e8fe9b3ee71127ef040d609724d1
Santiago <santiago.penino@gmail.com>
2026-02-20 14:31:19 -0300

    limpieza de archivos

--- scripts/legacy_migration/postcss.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    plugins: {
-        // With @tailwindcss/vite, we don't need tailwindcss or autoprefixer here 
-        // as the Vite plugin handles them.
-        // This file exists primarily to prevent picking up postcss.config.js 
-        // from parent directories.
-    },
-}
