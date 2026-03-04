f45c022c3def26a0fb06c652c1c3e6179684af0a
Santiago <santiago.penino@gmail.com>
2026-03-04 16:39:41 -0300

    carpeta react borrada

--- react/back/cache.js
+++
@@ -1,6 +0,0 @@
-const NodeCache = require('node-cache');
-
-// Crear una instancia de NodeCache
-const cache = new NodeCache({ stdTTL: 36000, checkperiod: 600 });
-
-module.exports = cache; // Exportar la instancia
