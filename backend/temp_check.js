7ad8c35271528649ab4d3817e9f9d5e61b56cf5b
Yoania <useryoania@gmail.com>
2026-04-24 16:15:27 -0300

    copia

--- backend/temp_check.js
+++ backend/temp_check.js
@@ -1,8 +1,8 @@
 const { getPool } = require('./config/db.js');
 async function main() {
   const pool = await getPool();
-  const r = await pool.request().query("SELECT name FROM sys.tables");
-  console.log('Tables:', r.recordset.map(x=>x.name).join(', '));
+  const r = await pool.request().query("SELECT p.CodArticulo, pl.Precio as PrecioBase, pl.Moneda as MonedaBase FROM dbo.Articulos p LEFT JOIN dbo.PreciosListaPublica pl ON p.ProIdProducto = pl.ProIdProducto WHERE p.CodStock IN ('2.2.1.1', '2.2.1.2')");
+  console.log(r.recordset.slice(0, 5));
   process.exit(0);
 }
 main().catch(console.error);
